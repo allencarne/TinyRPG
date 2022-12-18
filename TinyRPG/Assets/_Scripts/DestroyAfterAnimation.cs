3b158a858ffc78b21b372e891c2901a1da6ef0fd
allencarne <allencarne@yahoo.com>
2022-12-17 22:29:15 -0800

    Player and tile

---
+++ TinyRPG/Assets/_Scripts/DestroyAfterAnimation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterAnimation : MonoBehaviour
+{
+    public void AE_DestroyAfterAnimation()
+    {
+        Destroy(gameObject);
+    }
+}
