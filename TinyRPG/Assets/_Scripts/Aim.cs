b742b0edb4a043c22f2ac02dfb1dd7ebb73ae4ad
allencarne <allencarne@yahoo.com>
2022-12-24 19:22:19 -0800

    Player Run Animations

--- TinyRPG/Assets/_Scripts/Aim.cs
+++ TinyRPG/Assets/_Scripts/Aim.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Aim : MonoBehaviour
 {
-    Player2 player2;
     public float offset;
     public Transform firePoint;
     [SerializeField] GameObject player;
-
-    private void Awake()
-    {
-        player2 = player.GetComponent<Player2>();
-    }
 
     void Update()
     {
