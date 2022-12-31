81652e3c6254640a0b228f0df5830888c924efd6
allencarne <allencarne@yahoo.com>
2022-12-31 09:13:30 -0800

    Training Dummy

---
+++ TinyRPG/Assets/_Scripts/BasicAttack.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttack : MonoBehaviour
+{
+    [SerializeField] GameObject hitSpark;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Instantiate(hitSpark, collision.transform.position, collision.transform.rotation);
+            Debug.Log("HIT");
+        }
+    }
+}
---
+++ TinyRPG/Assets/_Scripts/TrainingDummy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingDummy : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
