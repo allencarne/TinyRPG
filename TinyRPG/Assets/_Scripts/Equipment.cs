3e31fc8728cad3bf5cd8325cf401f3c6271f9f35
allencarne <allencarne@yahoo.com>
2023-02-01 19:30:28 -0800

    Items

---
+++ TinyRPG/Assets/_Scripts/Equipment.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item
+{
+
+}
